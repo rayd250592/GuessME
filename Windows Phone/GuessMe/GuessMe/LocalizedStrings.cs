f09cf3ea54f413f2e41552b2db5dd3e54614ca1d
rayd25051992 <rayd_250592@outlook.com>
2014-07-05 23:17:29 +0100

    Windows Phone 8 Project created

---
+++ Windows Phone/GuessMe/GuessMe/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using GuessMe.Resources;
+
+namespace GuessMe
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
