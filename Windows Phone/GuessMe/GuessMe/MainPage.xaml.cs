c767f060aa96471d678839622022ad5a76472a5d
rayd25051992 <rayd_250592@outlook.com>
2014-10-17 03:18:39 +0100

    Coded and Tested. Just need to add images and upload

--- Windows Phone/GuessMe/GuessMe/MainPage.xaml.cs
+++ Windows Phone/GuessMe/GuessMe/MainPage.xaml.cs
@@ -15,18 +15,27 @@ namespace GuessMe
     {
 
         // Constructor
+        public int randomnumber;
+
 
         public int GenerateRandomNumber(int min, int max)
         {
             Random random = new Random();
             return random.Next(min, max);
 
+            
+
         }
+     
+
 
         public MainPage()
         {
             
             InitializeComponent();
+            randomnumber = GenerateRandomNumber(1, 100);
+            response.Visibility = System.Windows.Visibility.Collapsed;
+            userno.Visibility = System.Windows.Visibility.Collapsed;
           
 
             // Sample code to localize the ApplicationBar
@@ -34,15 +43,77 @@ namespace GuessMe
            
         }
 
-
+      
         
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
-            int userint = Convert.ToInt32(userno.Text);
-            
-            
-            
+
+            int userint;
+
+            if (int.TryParse(userno.Text, out userint))
+            {
+
+                userint = Convert.ToInt32(userno.Text);
+
+                int randomNumber = Convert.ToInt32(no.Text);
+
+                if (userint > randomNumber)
+                {
+                    response.Text = "Lower!";
+                }
+
+                else if (userint - randomNumber == 5)
+                {
+                    response.Text = "Close!";
+                }
+
+                else if (userint - randomNumber == 1)
+                {
+                    response.Text = "Really Close!";
+                }
+
+                else if (randomNumber > userint)
+                {
+                    response.Text = "Higher!";
+                }
+
+                else if (randomNumber - userint == -5)
+                {
+                    response.Text = "Close!";
+                }
+
+                else if (userint - randomNumber == -1)
+                {
+                    response.Text = "Really Close!";
+                }
+
+                else
+                {
+                    response.Text = "You got it!";
+                }
+
+
+                
+
+            }
+
+            else
+            {
+                MessageBoxResult result1 =
+  MessageBox.Show("Please enter a number",
+  "Enter a number", MessageBoxButton.OKCancel);
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int randomnumber = GenerateRandomNumber(1, 100);
+            no.Text = randomnumber.ToString();
+
+            response.Visibility = System.Windows.Visibility.Visible;
+            userno.Visibility = System.Windows.Visibility.Visible;
 
         }
 
